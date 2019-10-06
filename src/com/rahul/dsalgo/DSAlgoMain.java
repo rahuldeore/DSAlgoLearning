adfe88d636ce620426a9c7d1366337973bbd3a5b
RahulD <rahul.deore@hotmail.com>
2019-10-06 16:10:42 -0400

    Revert "Revert "Initial commit including fibo and factorial examples""
    This reverts commit 8173963b96ee357891eed19fa91903e66c8768f2.

---
+++ src/com/rahul/dsalgo/DSAlgoMain.java
@@ -0,0 +1,25 @@
+package com.rahul.dsalgo;
+
+import com.rahul.dsalgo.algorithm.Factorial;
+import com.rahul.dsalgo.algorithm.Fibonacci;
+
+/**
+ * @author Rahul
+ * */
+public class DSAlgoMain {
+
+    private static final int FACTORIAL_NUM=8;
+    private static final int FIBONACCI_NUM=8;
+
+    public static void main(String[] args) {
+
+        Factorial factorial = new Factorial();
+        System.out.println(factorial.factorial(FACTORIAL_NUM));
+        System.out.println(factorial.factorialIterative(FACTORIAL_NUM));
+
+        Fibonacci fibonacci = new Fibonacci();
+        System.out.println(fibonacci.recursiveFindFiboNumAt(FIBONACCI_NUM));
+        System.out.println(fibonacci.iterativeFindFiboNumAt(FIBONACCI_NUM));
+        fibonacci.printFiboSequence(FIBONACCI_NUM);
+    }
+}
